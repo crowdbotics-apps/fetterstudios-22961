13f61ef17b181d74e8183d96e7ec8c98fcd70c5f
Crowdbotics <team@crowdbotics.com>
2020-11-25 00:26:53 +0000

    add package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'ArticleList178439', human_name: 'Article List', access_route: 'ArticleList178439'},
 { name: 'ArticleList178438', human_name: 'Article List', access_route: 'ArticleList178438'},
 { name: 'BlankScreen2178437', human_name: 'Blank Screen 2', access_route: 'BlankScreen2178437'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import ArticleList178439Navigator from '../features/ArticleList178439/navigator';
 import ArticleList178438Navigator from '../features/ArticleList178438/navigator';
 import BlankScreen2178437Navigator from '../features/BlankScreen2178437/navigator';
 
@@ -15,6 +16,7 @@ import BlankScreen2178437Navigator from '../features/BlankScreen2178437/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+ArticleList178439: { screen: ArticleList178439Navigator },
 ArticleList178438: { screen: ArticleList178438Navigator },
 BlankScreen2178437: { screen: BlankScreen2178437Navigator },
 
